f3a42a2187bab3394c2d36b40c3265c61bd3c2df
seheonnn <ho78901@naver.com>
2024-01-15 22:19:48 +0900

    :sparkles: feat: Config 설정

---
+++ src/main/java/com/smunity/graduation/global/config/SecurityConfig.java
@@ -0,0 +1,67 @@
+package com.smunity.graduation.global.config;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	private final String[] swaggerUrls = {"/swagger-ui/**", "/v3/**"};
+	private final String[] authUrls = {"/", "/api/v1/accounts/register/**"};
+	private final String[] allowedUrls = Stream.concat(Arrays.stream(swaggerUrls), Arrays.stream(authUrls))
+		.toArray(String[]::new);
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+		return configuration.getAuthenticationManager();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+		http
+			.cors((cors) -> cors
+				.configurationSource(CorsConfig.apiConfigurationSource()));
+
+		// csrf disable
+		http
+			.csrf(AbstractHttpConfigurer::disable);
+
+		// form 로그인 방식 disable
+		http
+			.formLogin(AbstractHttpConfigurer::disable);
+
+		// http basic 인증 방식 disable
+		http
+			.httpBasic(AbstractHttpConfigurer::disable);
+
+		// 경로별 인가 작업
+		http
+			.authorizeHttpRequests((auth) -> auth
+				.requestMatchers(allowedUrls).permitAll()
+				.requestMatchers("/").permitAll()
+				.anyRequest().authenticated()
+			);
+
+		return http.build();
+	}
+}
