ffade1600cd0f21b968817b15cd0a5b69b8e13f6
hyunmin0317 <choihm9903@naver.com>
2024-01-18 22:50:00 +0900

    ✨ feat: Password Encoder 변경 (#3)

--- src/main/java/com/smunity/graduation/global/config/SecurityConfig.java
+++ src/main/java/com/smunity/graduation/global/config/SecurityConfig.java
@@ -1,8 +1,13 @@
 package com.smunity.graduation.global.config;
 
-import java.util.Arrays;
-import java.util.stream.Stream;
-
+import com.smunity.graduation.domain.accounts.jwt.filter.CustomLoginFilter;
+import com.smunity.graduation.domain.accounts.jwt.filter.CustomLogoutHandler;
+import com.smunity.graduation.domain.accounts.jwt.filter.JwtExceptionFilter;
+import com.smunity.graduation.domain.accounts.jwt.filter.JwtFilter;
+import com.smunity.graduation.domain.accounts.jwt.util.JwtUtil;
+import com.smunity.graduation.domain.accounts.jwt.util.RedisUtil;
+import com.smunity.graduation.global.config.encoder.Pbkdf2PasswordEncoder;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,101 +16,95 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
 
-import com.smunity.graduation.domain.accounts.jwt.filter.CustomLoginFilter;
-import com.smunity.graduation.domain.accounts.jwt.filter.CustomLogoutHandler;
-import com.smunity.graduation.domain.accounts.jwt.filter.JwtExceptionFilter;
-import com.smunity.graduation.domain.accounts.jwt.filter.JwtFilter;
-import com.smunity.graduation.domain.accounts.jwt.util.JwtUtil;
-import com.smunity.graduation.domain.accounts.jwt.util.RedisUtil;
-
-import lombok.RequiredArgsConstructor;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-	private final String[] swaggerUrls = {"/swagger-ui/**", "/v3/**"};
-	private final String[] authUrls = {"/", "/api/v1/accounts/**"};
-	private final String[] allowedUrls = Stream.concat(Arrays.stream(swaggerUrls), Arrays.stream(authUrls))
-		.toArray(String[]::new);
+    private final String[] swaggerUrls = {"/swagger-ui/**", "/v3/**"};
+    private final String[] authUrls = {"/", "/api/v1/accounts/**"};
+    private final String[] allowedUrls = Stream.concat(Arrays.stream(swaggerUrls), Arrays.stream(authUrls))
+            .toArray(String[]::new);
 
-	private final AuthenticationConfiguration authenticationConfiguration;
-	private final JwtUtil jwtUtil;
-	private final RedisUtil redisUtil;
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JwtUtil jwtUtil;
+    private final RedisUtil redisUtil;
 
-	@Bean
-	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-		return configuration.getAuthenticationManager();
-	}
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 
-	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new Pbkdf2PasswordEncoder();
+    }
 
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-		http
-			.cors((cors) -> cors
-				.configurationSource(CorsConfig.apiConfigurationSource()));
+        http
+                .cors((cors) -> cors
+                        .configurationSource(CorsConfig.apiConfigurationSource()));
 
-		// csrf disable
-		http
-			.csrf(AbstractHttpConfigurer::disable);
+        // csrf disable
+        http
+                .csrf(AbstractHttpConfigurer::disable);
 
-		// form 로그인 방식 disable
-		http
-			.formLogin(AbstractHttpConfigurer::disable);
+        // form 로그인 방식 disable
+        http
+                .formLogin(AbstractHttpConfigurer::disable);
 
-		// http basic 인증 방식 disable
-		http
-			.httpBasic(AbstractHttpConfigurer::disable);
+        // http basic 인증 방식 disable
+        http
+                .httpBasic(AbstractHttpConfigurer::disable);
 
-		// 경로별 인가 작업
-		http
-			.authorizeHttpRequests((auth) -> auth
-				.requestMatchers(allowedUrls).permitAll()
-				.requestMatchers("/").permitAll()
-				.anyRequest().authenticated()
-			);
+        // 경로별 인가 작업
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers(allowedUrls).permitAll()
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated()
+                );
 
-		// Jwt Filter (with login)
-		CustomLoginFilter loginFilter = new CustomLoginFilter(
-			authenticationManager(authenticationConfiguration), jwtUtil
-		);
-		loginFilter.setFilterProcessesUrl("/api/v1/accounts/login");
+        // Jwt Filter (with login)
+        CustomLoginFilter loginFilter = new CustomLoginFilter(
+                authenticationManager(authenticationConfiguration), jwtUtil
+        );
+        loginFilter.setFilterProcessesUrl("/api/v1/accounts/login");
 
-		http
-			.addFilterAt(loginFilter, UsernamePasswordAuthenticationFilter.class);
+        http
+                .addFilterAt(loginFilter, UsernamePasswordAuthenticationFilter.class);
 
-		http
-			.addFilterBefore(new JwtFilter(jwtUtil, redisUtil), CustomLoginFilter.class);
+        http
+                .addFilterBefore(new JwtFilter(jwtUtil, redisUtil), CustomLoginFilter.class);
 
-		// Exception Handle filter
-		http
-			.addFilterBefore(new JwtExceptionFilter(), LogoutFilter.class);
+        // Exception Handle filter
+        http
+                .addFilterBefore(new JwtExceptionFilter(), LogoutFilter.class);
 
-		// 세션 설정
-		http
-			.sessionManagement((session) -> session
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			);
+        // 세션 설정
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
 
-		// Logout Filter
-		http
-			.logout((logout) -> logout
-				.logoutUrl("/api/v1/accounts/logout")
-				.logoutSuccessUrl("/")
-				.addLogoutHandler(new CustomLogoutHandler(redisUtil, jwtUtil))
-			);
+        // Logout Filter
+        http
+                .logout((logout) -> logout
+                        .logoutUrl("/api/v1/accounts/logout")
+                        .logoutSuccessUrl("/")
+                        .addLogoutHandler(new CustomLogoutHandler(redisUtil, jwtUtil))
+                );
 
-		return http.build();
-	}
+        return http.build();
+    }
 }
