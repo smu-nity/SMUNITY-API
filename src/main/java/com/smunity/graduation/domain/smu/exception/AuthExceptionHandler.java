bee4b70a579c364c079c16b6a798bcee8e061ba6
hyunmin0317 <choihm9903@naver.com>
2024-01-21 12:19:57 +0900

    ✨ feat: 샘물 인증 관련 에러 핸들링 (#13)

--- src/main/java/com/smunity/graduation/domain/smu/exception/AuthExceptionHandler.java
+++ src/main/java/com/smunity/graduation/domain/smu/exception/AuthExceptionHandler.java
@@ -1,12 +1,14 @@
 package com.smunity.graduation.domain.smu.exception;
 
-import com.smunity.graduation.global.error.exception.RestException;
-import org.springframework.http.HttpStatus;
+import com.smunity.graduation.global.common.code.BaseErrorCode;
+import com.smunity.graduation.global.common.code.status.ErrorCode;
+import com.smunity.graduation.global.common.exception.GeneralException;
 import org.springframework.web.reactive.function.client.ClientResponse;
 import reactor.core.publisher.Mono;
 
 public class AuthExceptionHandler {
     public static Mono<? extends Throwable> handleError(ClientResponse response) {
-        return Mono.error(new RestException((HttpStatus) response.statusCode()));
+        BaseErrorCode code = (response.statusCode().value() == 401) ? ErrorCode.SMU_UNAUTHORIZED : ErrorCode.SMU_INTERNAL_SERVER_ERROR;
+        return Mono.error(new GeneralException(code));
     }
 }
