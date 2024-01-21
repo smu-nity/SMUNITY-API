43b65d3aaeb2fc994adcf3438cfa19c5fb931244
hyunmin0317 <choihm9903@naver.com>
2024-01-21 12:08:08 +0900

    ✨ feat: 샘물 인증 관련 에러 코드 작성 (#13)

--- src/main/java/com/smunity/graduation/global/common/code/status/ErrorCode.java
+++ src/main/java/com/smunity/graduation/global/common/code/status/ErrorCode.java
@@ -1,54 +1,55 @@
 package com.smunity.graduation.global.common.code.status;
 
-import org.springframework.http.HttpStatus;
-
 import com.smunity.graduation.global.common.code.BaseErrorCode;
 import com.smunity.graduation.global.common.code.ErrorReasonDTO;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
 public enum ErrorCode implements BaseErrorCode {
 
-	// 가장 일반적인 응답
-	_INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러, 관리자에게 문의 바랍니다."),
-	_BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON400", "잘못된 요청입니다."),
-	_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON401", "인증이 필요합니다."),
-	_FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
+    // 가장 일반적인 응답
+    _INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러, 관리자에게 문의 바랍니다."),
+    _BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON400", "잘못된 요청입니다."),
+    _UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON401", "인증이 필요합니다."),
+    _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
 
-	// 멤버 관련 에러
-	USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "USER4001", "사용자가 없습니다."),
-	NICKNAME_NOT_EXIST(HttpStatus.BAD_REQUEST, "USER4002", "닉네임은 필수 입니다."),
-	PASSWORD_NOT_EQUAL(HttpStatus.BAD_REQUEST, "USER4003", "비밀번호가 일치하지 않습니다."),
-	SAMNUL_ERROR(HttpStatus.BAD_REQUEST, "SAM4001", "샘물 에러입니다."),
+    // 멤버 관련 에러
+    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "USER4001", "사용자가 없습니다."),
+    NICKNAME_NOT_EXIST(HttpStatus.BAD_REQUEST, "USER4002", "닉네임은 필수 입니다."),
+    PASSWORD_NOT_EQUAL(HttpStatus.BAD_REQUEST, "USER4003", "비밀번호가 일치하지 않습니다."),
+    SAMNUL_ERROR(HttpStatus.BAD_REQUEST, "SAM4001", "샘물 에러입니다."),
 
-	// 예시,,,
-	ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "ARTICLE4001", "게시글이 없습니다."),
+    // 샘물 인증 관련 에러
+    SMU_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "SMU401", "아이디 및 비밀번호가 일치하지 않습니다."),
+    SMU_INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "SMU500", "모듈의 형식이 예상한 형식과 일치하지 않습니다. 관리자에게 문의해주세요!"),
 
-	// For test
-	TEMP_EXCEPTION(HttpStatus.BAD_REQUEST, "TEMP4001", "이거는 테스트");
+    // 예시,,,
+    ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "ARTICLE4001", "게시글이 없습니다."),
 
-	private final HttpStatus httpStatus;
-	private final String code;
-	private final String message;
+    // For test
+    TEMP_EXCEPTION(HttpStatus.BAD_REQUEST, "TEMP4001", "이거는 테스트");
 
-	@Override
-	public ErrorReasonDTO getReason() {
-		return ErrorReasonDTO.builder()
-			.message(message)
-			.code(code)
-			.build();
-	}
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
 
-	@Override
-	public ErrorReasonDTO getReasonHttpStatus() {
-		return ErrorReasonDTO.builder()
-			.message(message)
-			.code(code)
-			.httpStatus(httpStatus)
-			.build()
-			;
-	}
+    @Override
+    public ErrorReasonDTO getReason() {
+        return ErrorReasonDTO.builder()
+                .message(message)
+                .code(code)
+                .build();
+    }
+
+    @Override
+    public ErrorReasonDTO getReasonHttpStatus() {
+        return ErrorReasonDTO.builder()
+                .message(message)
+                .code(code)
+                .httpStatus(httpStatus)
+                .build();
+    }
 }
