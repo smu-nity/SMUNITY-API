43a6bf48bd4728075bee5d825bdd518c057b5a75
hyunmin0317 <choihm9903@naver.com>
2024-01-22 21:09:54 +0900

    ♻️ refactor: 코드 컨벤션 규칙에 맞게 리팩토링 (#13)

--- src/test/java/com/smunity/graduation/domain/auth/service/AuthServiceTest.java
+++ src/test/java/com/smunity/graduation/domain/auth/service/AuthServiceTest.java
@@ -12,9 +12,9 @@ import java.util.List;
 
 @SpringBootTest
 public class AuthServiceTest {
+
     @Autowired
     private AuthService authService;
-
     private AuthRequestDto requestDto;
 
     @BeforeEach
