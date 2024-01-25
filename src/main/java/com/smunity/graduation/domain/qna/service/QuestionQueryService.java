51225b75e18038b32cb117d9da745a66b52d8678
gomin0 <gomin0981113@naver.com>
2024-01-25 21:29:54 +0900

    :sparkles: feat: 페이징 기능 추가 (#22)

--- src/main/java/com/smunity/graduation/domain/qna/service/QuestionQueryService.java
+++ src/main/java/com/smunity/graduation/domain/qna/service/QuestionQueryService.java
@@ -4,6 +4,8 @@ import com.smunity.graduation.domain.qna.dto.QuestionsResponseDto;
 import com.smunity.graduation.domain.qna.entity.Question;
 import com.smunity.graduation.domain.qna.repository.QuestionJpaRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,9 +17,9 @@ import java.util.List;
 public class QuestionQueryService {
     private final QuestionJpaRepository questionJpaRepository;
 
-    public List<QuestionsResponseDto> getQuestionList() {
-        List<Question> questions = questionJpaRepository.findAll();
-        return QuestionsResponseDto.from(questions);
+    public Page<QuestionsResponseDto> getQuestionList(Pageable pageable) {
+        Page<Question> questions = questionJpaRepository.findAll(pageable);
+        return questions.map(QuestionsResponseDto::from);
     }
 
 }
