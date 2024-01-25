85bdff9b52e0d72eb44de3132560dd658a3599b1
gomin0 <gomin0981113@naver.com>
2024-01-25 21:08:10 +0900

    :recycle: refactor: 상태 enum 수정 (#22)

--- src/main/java/com/smunity/graduation/domain/qna/entity/QuestionStatus.java
+++ src/main/java/com/smunity/graduation/domain/qna/entity/QuestionStatus.java
@@ -1,5 +1,18 @@
 package com.smunity.graduation.domain.qna.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum QuestionStatus {
-    RECEIPT, COMPLETE
+    RECEIPT("접수"),
+    COMPLETE("답변완료"),
+    ;
+
+    private final String value;
+
+    public static QuestionStatus from(Question question) {
+        return question.getAnswer() != null ? COMPLETE : COMPLETE;
+    }
 }
