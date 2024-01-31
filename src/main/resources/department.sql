d158056bb2d8bff2df21644e0727ae7ce1246168
seheonnn <ho78901@naver.com>
2024-01-31 23:13:48 +0900

    :memo: Docs: sql 파일 추가

---
+++ src/main/resources/department.sql
@@ -0,0 +1,136 @@
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('융합공과대학', '컴퓨터과학전공', '공학',
+        'https://cs.smu.ac.kr/cs/admission/curriculum.do?&srYear=2023&srShyr=all&srSust=03005');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('융합공과대학', '전기공학전공', '자연',
+        'https://electric.smu.ac.kr/electric/admission/curriculum.do?&srYear=2023&srShyr=all&srSust=03208');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('융합공과대학', '지능IOT융합전공', '공학',
+        'https://www.smu.ac.kr/aiot/admission/curriculum.do?&srYear=2023&srShyr=all&srSust=03209');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('융합공과대학', '게임전공', '공학',
+        'https://game.smu.ac.kr/game01/admission/curriculum.do?&srYear=2023&srShyr=all&srSust=03006');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('융합공과대학', '애니메이션전공', '예술',
+        'https://animation.smu.ac.kr/animation/admission/curriculum.do?&srYear=2023&srShyr=all&srSust=03007');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('융합공과대학', '휴먼지능정보공학전공', '공학',
+        'https://hi.smu.ac.kr/hi/admissions/curriculum.do?&srYear=2023&srShyr=all&srSust=03204');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('융합공과대학', '핀테크전공', '공학',
+        'https://fbs.smu.ac.kr/fbs/admission/curriculum.do?&srYear=2023&srShyr=all&srSust=03205');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('융합공과대학', '빅데이터융합전공', '공학',
+        'https://fbs.smu.ac.kr/fbs/admission/curriculum_big.do?&srYear=2023&srShyr=all&srSust=03206');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('융합공과대학', '스마트생산전공', '공학',
+        'https://fbs.smu.ac.kr/fbs/admission/curriculum_smart.do?&srYear=2023&srShyr=all&srSust=03207');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('융합공과대학', '생명공학전공', '자연',
+        'https://biotechnology.smu.ac.kr/biotechnology/admission/curriculum.do?&srYear=2023&srShyr=all&srSust=0300');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('융합공과대학', '화학에너지공학전공', '자연',
+        'https://energy.smu.ac.kr/cee/admission/curriculum.do?&srYear=2023&srShyr=all&srSust=03001');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('융합공과대학', '화공신소재전공', '자연',
+        'https://ichem.smu.ac.kr/ichemistry/admission/curriculum.do?&srYear=2023&srShyr=all&srSust=03002');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('융합공과대학', '한일문화콘텐츠전공', '인문', 'https://kjc.smu.ac.kr/kjc/admission/curriculum.do?&srYear=2023&srShyr=all');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('인문사회과학대학', '역사콘텐츠전공', '인문',
+        'https://history.smu.ac.kr/history/admission/curriculum.do?&srYear=2023&srShyr=all&srSust=02988');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('인문사회과학대학', '지적재산권전공', '사회',
+        'https://cr.smu.ac.kr/cc/admission/curriculum.do?&srYear=2023&srShyr=all&srSust=02989');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('인문사회과학대학', '문헌정보학전공', '사회',
+        'https://libinfo.smu.ac.kr/libinfo/admission/curriculum.do?&srYear=2023&srShyr=all&srSust=02990');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('인문사회과학대학', '공간환경학부', '사회',
+        'https://space.smu.ac.kr/space/admission/curriculum.do?&srYear=2023&srShyr=all&srSust=00962');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('인문사회과학대학', '행정학부', '사회',
+        'https://public.smu.ac.kr/public/admission/curriculum.do?&srYear=2023&srShyr=all&srSust=03183');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('인문사회과학대학', '가족복지학과', '사회',
+        'https://smfamily.smu.ac.kr/smfamily/admission/curriculum.do?&srYear=2023&srShyr=all&srSust=00951');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('인문사회과학대학', '국가안보학과', '인문',
+        'https://ns.smu.ac.kr/sdms/admission/curriculum.do?&srYear=2023&srShyr=all&srSust=00964');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('사범대학', '국어교육과', '인문',
+        'https://koredu.smu.ac.kr/koredu/admission/curriculum.do?&srYear=2023&srShyr=all&srSust=00971');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('사범대학', '영어교육과', '인문',
+        'https://engedu.smu.ac.kr/engedu/admission/curriculum.do?&srYear=2023&srShyr=all&srSust=00972');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('사범대학', '교육학과', '인문',
+        'https://learning.smu.ac.kr/peda/admission/curriculum.do?&srYear=2023&srShyr=all&srSust=00975');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('사범대학', '수학교육과', '자연',
+        'https://mathed.smu.ac.kr/mathedu/admission/curriculum.do?&srYear=2023&srShyr=all&srSust=00976');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('경영경제대학', '경제금융학부', '사회',
+        'https://econo.smu.ac.kr/economic/admission/curriculum.do?&srYear=2023&srShyr=all&srSust=01178');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('경영경제대학', '경영학부', '사회', 'https://smubiz.smu.ac.kr/smubiz/admission/grade.do');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('경영경제대학', '글로벌경영학과', '사회', 'https://gbiz.smu.ac.kr/newmajoritb/life/subject.do');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('경영경제대학', '융합경영학과', '사회',
+        'https://imgmt.smu.ac.kr/cm/admission/curriculum.do?&srYear=2023&srShyr=all&srSust=01181');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('문화예술대학', '식품영양학전공', '자연',
+        'https://food.smu.ac.kr/foodnutrition/admission/curriculum.do?&srYear=2023&srShyr=all&srSust=02994');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('문화예술대학', '의류학전공', '자연',
+        'https://fashionindustry.smu.ac.kr/clothing2/admission/curriculum.do?srYear=2023&srSust=02995&srShyr=all');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('문화예술대학', '스포츠건강관리전공', '예술',
+        'https://smpe.smu.ac.kr/smpe/admission/curriculum.do?srYear=2023&srSust=02997&srShyr=all');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('문화예술대학', '무용예술전공', '예술',
+        'https://dance.smu.ac.kr/dance/undergraduate/undergraduate_curriculum.do?&srYear=2023&srShyr=all&srSust=02998');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('문화예술대학', '조형예술전공', '예술',
+        'https://finearts.smu.ac.kr/finearts/admission/curriculum.do?&srYear=2023&srShyr=all&srSust=02991');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('문화예술대학', '생활예술전공', '예술',
+        'https://smulad.smu.ac.kr/smulad/admission/curriculum.do?&srYear=2023&srShyr=all&srSust=02992');
+
+INSERT INTO accounts_department(college, name, type, url)
+VALUES ('문화예술대학', '음악학부', '예술',
+        'https://music.smu.ac.kr/music/admission/curriculum.do?&srYear=2023&srShyr=all&srSust=01132');
