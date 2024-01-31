f5361f41ce0d89dffb3ac37317596b57a561d60d
hyunmin0317 <choihm9903@naver.com>
2024-01-31 21:52:33 +0900

    ✨ feat: 기이수과목 업데이트 API 작성 (#30)

--- src/main/java/com/smunity/graduation/domain/course/controller/CourseController.java
+++ src/main/java/com/smunity/graduation/domain/course/controller/CourseController.java
@@ -3,6 +3,7 @@ package com.smunity.graduation.domain.course.controller;
 import com.smunity.graduation.domain.auth.dto.AuthCourseResponseDto;
 import com.smunity.graduation.domain.auth.dto.AuthRequestDto;
 import com.smunity.graduation.domain.auth.service.AuthService;
+import com.smunity.graduation.domain.course.dto.CourseResponseDto;
 import com.smunity.graduation.domain.course.service.CourseService;
 import com.smunity.graduation.global.common.ApiResponse;
 import jakarta.validation.Valid;
@@ -18,15 +19,14 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/courses")
 public class CourseController {
-    
+
     private final AuthService authService;
     private final CourseService courseService;
 
     @PostMapping("/upload")
-    public ApiResponse<List<AuthCourseResponseDto>> uploadCourses(@RequestBody @Valid AuthRequestDto requestDto) {
+    public ApiResponse<List<CourseResponseDto>> uploadCourses(@RequestBody @Valid AuthRequestDto requestDto) {
         List<AuthCourseResponseDto> requestDtoList = authService.getCourses(requestDto);
-        // TODO return 타입 List<AuthCourseResponseDto> -> List<CourseResponseDto>
-        List<AuthCourseResponseDto> responseDtoList = courseService.createCourses(requestDtoList);
+        List<CourseResponseDto> responseDtoList = courseService.createCourses(requestDtoList);
         return ApiResponse.onSuccess(responseDtoList);
     }
 }
--- src/main/java/com/smunity/graduation/domain/course/service/CourseService.java
+++ src/main/java/com/smunity/graduation/domain/course/service/CourseService.java
@@ -1,6 +1,15 @@
 package com.smunity.graduation.domain.course.service;
 
+import com.smunity.graduation.domain.accounts.entity.User;
+import com.smunity.graduation.domain.accounts.repository.user.UserRepository;
 import com.smunity.graduation.domain.auth.dto.AuthCourseResponseDto;
+import com.smunity.graduation.domain.course.dto.CourseResponseDto;
+import com.smunity.graduation.domain.course.entity.Course;
+import com.smunity.graduation.domain.course.repository.CourseRepository;
+import com.smunity.graduation.domain.graduation.entity.Subject;
+import com.smunity.graduation.domain.graduation.repository.SubjectRepository;
+import com.smunity.graduation.global.common.code.status.ErrorCode;
+import com.smunity.graduation.global.common.exception.GeneralException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,19 +19,24 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class CourseService {
-    
-    // TODO return 타입 AuthCourseResponseDto -> CourseResponseDto
-    public AuthCourseResponseDto createCourse(AuthCourseResponseDto requestDto) {
-        // TODO requestDto -> entity (toEntity)
+    private final UserRepository userRepository;
+    private final SubjectRepository subjectRepository;
+    private final CourseRepository courseRepository;
 
-        // TODO entity save
-
-        // TODO entity -> responseDto (from)
-
-        return requestDto;
+    public CourseResponseDto createCourse(AuthCourseResponseDto requestDto) {
+        // TODO 사용자 인증 적용
+        User user = userRepository.findByUserName("201911019")
+                .orElseThrow(() -> new GeneralException(ErrorCode._UNAUTHORIZED));
+        Subject subject = subjectRepository.findByNumber(requestDto.number())
+                .orElseThrow(() -> new GeneralException(ErrorCode.SUBJECT_NOT_FOUND));
+        Course course = requestDto.toEntity();
+        course.setUser(user);
+        course.setSubject(subject);
+        Course saved = courseRepository.save(course);
+        return CourseResponseDto.from(saved);
     }
 
-    public List<AuthCourseResponseDto> createCourses(List<AuthCourseResponseDto> requestDtoList) {
+    public List<CourseResponseDto> createCourses(List<AuthCourseResponseDto> requestDtoList) {
         return requestDtoList.stream().map(this::createCourse).collect(Collectors.toList());
     }
 }
