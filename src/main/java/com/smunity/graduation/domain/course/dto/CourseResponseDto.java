dece9e653d61749cdb15cfbace575db0b9a37972
hyunmin0317 <choihm9903@naver.com>
2024-01-31 21:52:07 +0900

    ✨ feat: from 메서드 작성 (#30)

--- src/main/java/com/smunity/graduation/domain/course/dto/CourseResponseDto.java
+++ src/main/java/com/smunity/graduation/domain/course/dto/CourseResponseDto.java
@@ -1,15 +1,32 @@
 package com.smunity.graduation.domain.course.dto;
 
+import com.smunity.graduation.domain.course.entity.Course;
+import lombok.Builder;
+
+@Builder
 public record CourseResponseDto(
         Long id,
-        Long subjectId,
-        Long userId,
         String year,
         String semester,
+        String number,
+        String name,
         String type,
         String domain,
         int credit,
         boolean custom
 ) {
-    
+
+    public static CourseResponseDto from(Course course) {
+        return CourseResponseDto.builder()
+                .id(course.getId())
+                .year(course.getYear())
+                .semester(course.getSemester())
+                .number(course.getSubject().getNumber())
+                .name(course.getSubject().getName())
+                .type(course.getType())
+                .domain(course.getDomain())
+                .credit(course.getCredit())
+                .custom(course.isCustom())
+                .build();
+    }
 }
