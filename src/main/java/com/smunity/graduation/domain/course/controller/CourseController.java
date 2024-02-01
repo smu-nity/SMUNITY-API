72e3c7803894946c0acb36f9d78c08d4d3eea391
hyunmin0317 <choihm9903@naver.com>
2024-02-01 22:00:11 +0900

    ✨ feat: 기이수과목 API 작성 (#30)

--- src/main/java/com/smunity/graduation/domain/course/controller/CourseController.java
+++ src/main/java/com/smunity/graduation/domain/course/controller/CourseController.java
@@ -4,14 +4,12 @@ import com.smunity.graduation.domain.auth.dto.AuthCourseResponseDto;
 import com.smunity.graduation.domain.auth.dto.AuthRequestDto;
 import com.smunity.graduation.domain.auth.service.AuthService;
 import com.smunity.graduation.domain.course.dto.CourseResponseDto;
+import com.smunity.graduation.domain.course.service.CourseQueryService;
 import com.smunity.graduation.domain.course.service.CourseService;
 import com.smunity.graduation.global.common.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,11 +20,22 @@ public class CourseController {
 
     private final AuthService authService;
     private final CourseService courseService;
+    private final CourseQueryService courseQueryService;
+
+    @GetMapping
+    public ApiResponse<List<CourseResponseDto>> getCourses() {
+        // TODO 사용자 인증 적용
+        String username = "201911019";
+        List<CourseResponseDto> responseDtoList = courseQueryService.getCourses(username);
+        return ApiResponse.onSuccess(responseDtoList);
+    }
 
     @PostMapping("/upload")
     public ApiResponse<List<CourseResponseDto>> uploadCourses(@RequestBody @Valid AuthRequestDto requestDto) {
         List<AuthCourseResponseDto> requestDtoList = authService.getCourses(requestDto);
-        List<CourseResponseDto> responseDtoList = courseService.createCourses(requestDtoList);
+        // TODO 사용자 인증 적용
+        String username = "201911019";
+        List<CourseResponseDto> responseDtoList = courseService.createCourses(requestDtoList, username);
         return ApiResponse.onSuccess(responseDtoList);
     }
 }
