5665f5b5c83b2c588bcc2e5a97a1c5925d46fda7
hyunmin0317 <choihm9903@naver.com>
2024-02-10 19:30:29 +0900

    ✨ feat: AuthResponseDto 수정 (#47)

--- src/main/java/com/smunity/graduation/domain/auth/dto/AuthResponseDto.java
+++ src/main/java/com/smunity/graduation/domain/auth/dto/AuthResponseDto.java
@@ -3,7 +3,9 @@ package com.smunity.graduation.domain.auth.dto;
 public record AuthResponseDto(
         String name,
         String department,
-        String email
+        String email,
+        int year,
+        int semester
 ) {
-    
+
 }
