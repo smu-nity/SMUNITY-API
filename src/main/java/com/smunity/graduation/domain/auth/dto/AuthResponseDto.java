0a05951f910b9260abb11e7869ed6d0f278cbcb1
hyunmin0317 <choihm9903@naver.com>
2024-02-15 20:20:22 +0900

    ✨ feat: 학번 필드 추가 (#47)

--- src/main/java/com/smunity/graduation/domain/auth/dto/AuthResponseDto.java
+++ src/main/java/com/smunity/graduation/domain/auth/dto/AuthResponseDto.java
@@ -2,6 +2,7 @@ package com.smunity.graduation.domain.auth.dto;
 
 public record AuthResponseDto(
         String name,
+        String username,
         String department,
         String email,
         int year,
