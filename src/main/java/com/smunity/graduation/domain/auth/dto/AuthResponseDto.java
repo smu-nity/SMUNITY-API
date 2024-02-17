4b6b06335710734a5db6288315d3d3760b1f6232
hyunmin0317 <choihm9903@naver.com>
2024-02-17 16:14:09 +0900

    ✨ feat: authenticate 메서드 수정 (#54)

--- src/main/java/com/smunity/graduation/domain/auth/dto/AuthResponseDto.java
+++ src/main/java/com/smunity/graduation/domain/auth/dto/AuthResponseDto.java
@@ -1,5 +1,9 @@
 package com.smunity.graduation.domain.auth.dto;
 
+import lombok.Builder;
+import org.json.JSONObject;
+
+@Builder
 public record AuthResponseDto(
         String name,
         String username,
@@ -9,4 +13,20 @@ public record AuthResponseDto(
         int semester
 ) {
 
+    public static AuthResponseDto from(JSONObject response) {
+        JSONObject res = response.getJSONArray("dsStdInfoList").getJSONObject(0);
+        return AuthResponseDto.builder()
+                .name(res.getString("NM_KOR"))
+                .username(res.getString("STDNO"))
+                .department(getDepartment(res.getString("TMP_DEPT_MJR_NM")))
+                .email(res.getString("EMAIL"))
+                .year(res.getInt("SHYR"))
+                .semester(res.getInt("CMP_SMT"))
+                .build();
+    }
+
+    public static String getDepartment(String dept) {
+        String[] depts = dept.split(" ");
+        return depts[depts.length - 1];
+    }
 }
--- src/main/java/com/smunity/graduation/domain/auth/service/AuthService.java
+++ src/main/java/com/smunity/graduation/domain/auth/service/AuthService.java
@@ -4,28 +4,35 @@ import com.smunity.graduation.domain.auth.dto.AuthCourseResponseDto;
 import com.smunity.graduation.domain.auth.dto.AuthRequestDto;
 import com.smunity.graduation.domain.auth.dto.AuthResponseDto;
 import com.smunity.graduation.domain.auth.exception.AuthExceptionHandler;
+import com.smunity.graduation.global.common.ErrorCode;
+import com.smunity.graduation.global.common.exception.CustomException;
 import lombok.RequiredArgsConstructor;
+import org.json.JSONObject;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
-    
+
     private final WebClient webClient;
+    private final String LOGIN_URL = "https://smsso.smu.ac.kr/Login.do";
+    private final String BASE_URL = "https://smul.smu.ac.kr";
 
     public AuthResponseDto authenticate(AuthRequestDto requestDto) {
-        return webClient.post()
-                .uri("/api/userinfo")
-                .bodyValue(requestDto)
-                .retrieve()
-                .onStatus(HttpStatusCode::isError, AuthExceptionHandler::handleError)
-                .toEntity(AuthResponseDto.class)
-                .block()
-                .getBody();
+        JSONObject response = getData(requestDto, "/UsrSchMng/selectStdInfo.do");
+        return AuthResponseDto.from(response);
     }
 
     public List<AuthCourseResponseDto> getCourses(AuthRequestDto requestDto) {
@@ -37,5 +44,48 @@ public class AuthService {
                 .toEntityList(AuthCourseResponseDto.class)
                 .block()
                 .getBody();
+    }
+
+    private Map<String, String> login(AuthRequestDto requestDto) {
+        try {
+            Connection.Response response = Jsoup.connect(LOGIN_URL)
+                    .data("user_id", requestDto.username())
+                    .data("user_password", requestDto.password())
+                    .method(Connection.Method.POST)
+                    .execute();
+            if (response.url().toString().equals(LOGIN_URL))
+                throw new CustomException(ErrorCode.AUTH_UNAUTHORIZED);
+            return Jsoup.connect(BASE_URL.concat("/index.do"))
+                    .method(Connection.Method.GET)
+                    .cookies(response.cookies())
+                    .execute()
+                    .cookies();
+        } catch (IOException e) {
+            throw new CustomException(ErrorCode.AUTH_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private JSONObject getData(AuthRequestDto requestDto, String url) {
+        Map<String, String> session = login(requestDto);
+        try {
+            URL apiUrl = new URL(BASE_URL.concat(url));
+            HttpURLConnection connection = (HttpURLConnection) apiUrl.openConnection();
+            connection.setRequestMethod("POST");
+            for (Map.Entry<String, String> cookie : session.entrySet())
+                connection.addRequestProperty("Cookie", cookie.getKey() + "=" + cookie.getValue());
+            String requestData = "@d#=@d1#&@d1#tp=dm&_AUTH_MENU_KEY=usrCPsnlInfoUpd-STD&@d1#strStdNo=".concat(requestDto.username());
+            connection.setDoOutput(true);
+            connection.getOutputStream().write(requestData.getBytes());
+
+            StringBuilder response = new StringBuilder();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                String line;
+                while ((line = reader.readLine()) != null)
+                    response.append(line);
+            }
+            return new JSONObject(response.toString());
+        } catch (IOException e) {
+            throw new CustomException(ErrorCode.AUTH_INTERNAL_SERVER_ERROR);
+        }
     }
 }
