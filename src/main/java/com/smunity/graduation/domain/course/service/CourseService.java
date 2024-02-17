ee04f242a11c638473b3e1191f6ccb2577103471
hyunmin0317 <choihm9903@naver.com>
2024-02-17 18:05:53 +0900

    🐛 fix: 학점이 F인 과목 포함 오류 (#56)

--- src/main/java/com/smunity/graduation/domain/course/service/CourseService.java
+++ src/main/java/com/smunity/graduation/domain/course/service/CourseService.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -24,7 +25,10 @@ public class CourseService {
     private final CourseRepository courseRepository;
 
     public List<CourseResponseDto> createCourses(List<AuthCourseResponseDto> requestDtoList, String username) {
-        return requestDtoList.stream().map(dto -> createCourse(dto, username)).toList();
+        return requestDtoList.stream()
+                .filter(dto -> !Objects.equals(dto.grade(), "F"))
+                .map(dto -> createCourse(dto, username))
+                .toList();
     }
 
     private CourseResponseDto createCourse(AuthCourseResponseDto requestDto, String username) {
