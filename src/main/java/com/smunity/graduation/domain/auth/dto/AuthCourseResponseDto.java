c7f1774478663c786c91f05791aba29aab7c26f1
최현민 <choihm9903@naver.com>
2024-02-21 19:08:37 +0900

    [FEATURE] 재학생 인증 API 업데이트 (#54)
    * 🚀 chore: 인증 관련 의존성 추가 (#54)
    
    * ✨ feat: authenticate 메서드 수정 (#54)
    
    * ✨ feat: 재학생 인증 API 업데이트 (#54)
    
    * ♻️ refactor: dto 변환 로직 리팩토링 (#54)
    
    * 🔥 delete: WebClientConfig 삭제 (#54)

--- src/main/java/com/smunity/graduation/domain/auth/dto/AuthCourseResponseDto.java
+++ src/main/java/com/smunity/graduation/domain/auth/dto/AuthCourseResponseDto.java
@@ -1,7 +1,15 @@
 package com.smunity.graduation.domain.auth.dto;
 
 import com.smunity.graduation.domain.course.entity.Course;
+import lombok.Builder;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.IntStream;
+
+@Builder
 public record AuthCourseResponseDto(
         String number,
         String name,
@@ -12,6 +20,29 @@ public record AuthCourseResponseDto(
         String domain,
         int credit
 ) {
+
+    public static List<AuthCourseResponseDto> from(JSONArray objs) {
+        return IntStream.range(0, objs.length())
+                .mapToObj(i -> from(objs.getJSONObject(i)))
+                .toList();
+    }
+
+    public static AuthCourseResponseDto from(JSONObject obj) {
+        return AuthCourseResponseDto.builder()
+                .number(obj.getString("SBJ_NO"))
+                .name(obj.getString("SBJ_NM"))
+                .type(obj.getString("CMP_DIV_NM"))
+                .grade(obj.getString("GRD_NM"))
+                .year(obj.getString("SCH_YEAR"))
+                .semester(obj.getString("SMT_NM"))
+                .domain(getDomain(obj.getString("CULT_ARA_NM")))
+                .credit(obj.getInt("CDT"))
+                .build();
+    }
+
+    public static String getDomain(String domain) {
+        return !Objects.equals(domain, "*") ? domain : null;
+    }
 
     public Course toEntity() {
         return Course.builder()
--- src/main/java/com/smunity/graduation/domain/auth/dto/AuthResponseDto.java
+++ src/main/java/com/smunity/graduation/domain/auth/dto/AuthResponseDto.java
@@ -1,5 +1,10 @@
 package com.smunity.graduation.domain.auth.dto;
 
+import lombok.Builder;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+@Builder
 public record AuthResponseDto(
         String name,
         String username,
@@ -9,4 +14,23 @@ public record AuthResponseDto(
         int semester
 ) {
 
+    public static AuthResponseDto from(JSONArray json) {
+        return from(json.getJSONObject(0));
+    }
+
+    public static AuthResponseDto from(JSONObject obj) {
+        return AuthResponseDto.builder()
+                .name(obj.getString("NM_KOR"))
+                .username(obj.getString("STDNO"))
+                .department(getDepartment(obj.getString("TMP_DEPT_MJR_NM")))
+                .email(obj.getString("EMAIL"))
+                .year(obj.getInt("SHYR"))
+                .semester(obj.getInt("CMP_SMT"))
+                .build();
+    }
+
+    public static String getDepartment(String dept) {
+        String[] depts = dept.split(" ");
+        return depts[depts.length - 1];
+    }
 }
