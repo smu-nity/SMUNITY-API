a9e6b59ee5f28a1af3d53a5719faeb1d980d1a99
hyunmin0317 <choihm9903@naver.com>
2024-04-01 22:41:53 +0900

    ✨ feat: Course 엔티티 필드 추가 (#70)

--- src/main/java/com/smunity/graduation/domain/course/entity/Course.java
+++ src/main/java/com/smunity/graduation/domain/course/entity/Course.java
@@ -1,6 +1,8 @@
 package com.smunity.graduation.domain.course.entity;
 
 import com.smunity.graduation.domain.accounts.entity.User;
+import com.smunity.graduation.global.common.enums.Category;
+import com.smunity.graduation.global.common.enums.SubDomain;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -36,6 +38,11 @@ public class Course {
     private String type;
 
     private String domain;
+
+    @Column(nullable = false)
+    private Category category;
+
+    private SubDomain subDomain;
 
     @Column(nullable = false)
     private int credit;
