fc2b825a88e25f09a842a159e819b9d85bf3dcbe
hyunmin0317 <choihm9903@naver.com>
2024-04-01 23:10:57 +0900

    ♻️ refactor: Category of 메서드 리팩토링 (#70)

--- src/main/java/com/smunity/graduation/global/common/enums/Category.java
+++ src/main/java/com/smunity/graduation/global/common/enums/Category.java
@@ -3,9 +3,7 @@ package com.smunity.graduation.global.common.enums;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import java.util.EnumSet;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 @Getter
 @RequiredArgsConstructor
@@ -17,20 +15,12 @@ public enum Category {
     CULTURE_OPTIONAL("교선"),
     ETC("기타");
 
-    private static final Map<String, Category> lookup = new HashMap<>();
-
-    static {
-        for (Category pc : EnumSet.allOf(Category.class)) {
-            lookup.put(pc.getName(), pc);
-        }
-    }
-
     private final String name;
 
     public static Category of(String name) {
-        if (!lookup.containsKey(name)) {
-            return ETC;
-        }
-        return lookup.get(name);
+        return Arrays.stream(Category.values())
+                .filter(category -> name.contains(category.getName()))
+                .findFirst()
+                .orElse(ETC);
     }
 }
