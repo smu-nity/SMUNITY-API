ad694bbe0ede2b95d28fcaafe7607ded6d45c76e
hyunmin0317 <choihm9903@naver.com>
2024-04-01 23:01:48 +0900

    ✨ feat: Category of 메서드 작성 (#70)

--- src/main/java/com/smunity/graduation/global/common/enums/Category.java
+++ src/main/java/com/smunity/graduation/global/common/enums/Category.java
@@ -3,15 +3,34 @@ package com.smunity.graduation.global.common.enums;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+
 @Getter
 @RequiredArgsConstructor
 public enum Category {
-    
+
     MAJOR_ADVANCED("1전심"),
     MAJOR_OPTIONAL("1전선"),
     CULTURE_REQUIRED("교필"),
     CULTURE_OPTIONAL("교선"),
     ETC("기타");
 
+    private static final Map<String, Category> lookup = new HashMap<>();
+
+    static {
+        for (Category pc : EnumSet.allOf(Category.class)) {
+            lookup.put(pc.getName(), pc);
+        }
+    }
+
     private final String name;
+
+    public static Category of(String name) {
+        if (!lookup.containsKey(name)) {
+            return ETC;
+        }
+        return lookup.get(name);
+    }
 }
