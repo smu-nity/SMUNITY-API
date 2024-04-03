de6566502a8af5dcc0d8ca780e8932d1bf072fcd
hyunmin0317 <choihm9903@naver.com>
2024-04-03 22:33:05 +0900

    ✅ test: 동적 쿼리 메서드 테스트 (#74)

--- src/test/java/com/smunity/graduation/domain/course/repository/course/CourseRepositoryTest.java
+++ src/test/java/com/smunity/graduation/domain/course/repository/course/CourseRepositoryTest.java
@@ -7,11 +7,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
+import static com.smunity.graduation.global.common.enums.Category.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 public class CourseRepositoryTest {
 
     @Autowired
     CourseRepository courseRepository;
+
+    private static int cal(List<Course> courses) {
+        return courses.stream().mapToInt(Course::getCredit).sum();
+    }
 
     @Test
     public void findAllTest() throws Exception {
@@ -22,5 +29,23 @@ public class CourseRepositoryTest {
         for (Course course : courseList) {
             System.out.println(course);
         }
+    }
+
+    @Test
+    public void findByCategory() throws Exception {
+        //given
+        String userName = "201911019";
+
+        //when
+        List<Course> all = courseRepository.findByUsernameAndCategory(userName, null);
+        List<Course> majorAdvanced = courseRepository.findByUsernameAndCategory(userName, MAJOR_ADVANCED);
+        List<Course> majorOptional = courseRepository.findByUsernameAndCategory(userName, MAJOR_OPTIONAL);
+        List<Course> culture = courseRepository.findByUsernameAndCategory(userName, CULTURE);
+
+        //then
+        assertEquals(132, cal(all), "전체 이수 학점 오류");
+        assertEquals(18, cal(majorAdvanced), "전공 심화 이수 학점 오류");
+        assertEquals(66, cal(majorOptional), "전공 선택 이수 학점 오류");
+        assertEquals(42, cal(culture), "교양 이수 학점 오류");
     }
 }
