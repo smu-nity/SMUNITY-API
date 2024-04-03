5bbc8df1563b126e8797163344847ef707369dbd
hyunmin0317 <choihm9903@naver.com>
2024-04-03 21:56:05 +0900

    ✨ feat: Standard 리포지토리 작성 (#74)

---
+++ src/main/java/com/smunity/graduation/domain/course/repository/StandardRepository.java
@@ -0,0 +1,8 @@
+package com.smunity.graduation.domain.course.repository;
+
+import com.smunity.graduation.domain.course.entity.Standard;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StandardRepository extends JpaRepository<Standard, Long> {
+
+}
