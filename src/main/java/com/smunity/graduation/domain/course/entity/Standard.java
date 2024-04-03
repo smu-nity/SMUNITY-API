f0cf31f7157261227a0aa7cd5f906dd03cd488d1
hyunmin0317 <choihm9903@naver.com>
2024-04-03 21:54:38 +0900

    ✨ feat: Standard 엔티티 작성 (#74)

---
+++ src/main/java/com/smunity/graduation/domain/course/entity/Standard.java
@@ -0,0 +1,27 @@
+package com.smunity.graduation.domain.course.entity;
+
+import com.smunity.graduation.domain.accounts.entity.Year;
+import com.smunity.graduation.global.common.enums.Category;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "core_standard")
+public class Standard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "year_id")
+    private Year year;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @Column(nullable = false)
+    private int total;
+}
