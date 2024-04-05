07da8f56f09f98d56ec07227c96df3597eda1bd4
hyunmin0317 <choihm9903@naver.com>
2024-04-05 10:39:43 +0900

    ✨ feat: Curriculum 엔티티 작성 (#76)

---
+++ src/main/java/com/smunity/graduation/domain/course/entity/Curriculum.java
@@ -0,0 +1,29 @@
+package com.smunity.graduation.domain.course.entity;
+
+import com.smunity.graduation.domain.accounts.entity.Year;
+import com.smunity.graduation.global.common.enums.Domain;
+import com.smunity.graduation.global.common.enums.SubDomain;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "core_curriculum")
+public class Curriculum {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "year_id")
+    private Year year;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Domain domain;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private SubDomain subDomain;
+}
