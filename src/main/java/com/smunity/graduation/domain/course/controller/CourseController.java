a0e5fdbb1fc8769f04e036cdc55ae7eb97cde044
hyunmin0317 <choihm9903@naver.com>
2024-04-06 19:30:54 +0900

    ✨ feat: 이수현황 조회 API 구현 (#76)

--- src/main/java/com/smunity/graduation/domain/course/controller/CourseController.java
+++ src/main/java/com/smunity/graduation/domain/course/controller/CourseController.java
@@ -10,6 +10,7 @@ import com.smunity.graduation.domain.course.service.CourseQueryService;
 import com.smunity.graduation.domain.course.service.CourseService;
 import com.smunity.graduation.global.common.ApiResponse;
 import com.smunity.graduation.global.common.enums.Category;
+import com.smunity.graduation.global.common.enums.Domain;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -35,6 +36,12 @@ public class CourseController {
     public ApiResponse<ResultResponseDto> uploadCourses(@AccountResolver User user, @RequestBody @Valid AuthRequestDto requestDto) {
         List<AuthCourseResponseDto> requestDtoList = authService.getCourses(requestDto);
         ResultResponseDto responseDtoList = courseService.createCourses(requestDtoList, user.getUserName());
+        return ApiResponse.onSuccess(responseDtoList);
+    }
+
+    @GetMapping("/cultures/{domain}")
+    public ApiResponse<ResultResponseDto> getCultureCourses(@AccountResolver User user, @PathVariable Domain domain) {
+        ResultResponseDto responseDtoList = courseQueryService.getCultureCourses(user.getUserName(), domain);
         return ApiResponse.onSuccess(responseDtoList);
     }
 }
