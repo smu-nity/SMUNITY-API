7f8cd8d7874c7352970564fa60e4cf4dbe132967
hyunmin0317 <choihm9903@naver.com>
2024-04-06 17:56:34 +0900

    ✨ feat: getCultureCourses 메서드 작성 (#76)

--- src/main/java/com/smunity/graduation/domain/course/service/CourseQueryService.java
+++ src/main/java/com/smunity/graduation/domain/course/service/CourseQueryService.java
@@ -4,11 +4,16 @@ import com.smunity.graduation.domain.accounts.entity.User;
 import com.smunity.graduation.domain.accounts.repository.user.UserRepository;
 import com.smunity.graduation.domain.course.dto.ResultResponseDto;
 import com.smunity.graduation.domain.course.entity.Course;
+import com.smunity.graduation.domain.course.entity.Curriculum;
 import com.smunity.graduation.domain.course.entity.Standard;
+import com.smunity.graduation.domain.course.entity.SubDomainHolder;
+import com.smunity.graduation.domain.course.repository.CurriculumRepository;
 import com.smunity.graduation.domain.course.repository.StandardRepository;
 import com.smunity.graduation.domain.course.repository.course.CourseRepository;
 import com.smunity.graduation.global.common.ErrorCode;
 import com.smunity.graduation.global.common.enums.Category;
+import com.smunity.graduation.global.common.enums.Domain;
+import com.smunity.graduation.global.common.enums.SubDomain;
 import com.smunity.graduation.global.common.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -24,6 +29,7 @@ public class CourseQueryService {
     private final UserRepository userRepository;
     private final CourseRepository courseRepository;
     private final StandardRepository standardRepository;
+    private final CurriculumRepository curriculumRepository;
 
     public ResultResponseDto getCourses(String username, Category category) {
         User user = userRepository.findByUserName(username)
@@ -33,5 +39,19 @@ public class CourseQueryService {
                 .orElseGet(() -> user.getYear().getTotal());
         List<Course> courses = courseRepository.findByUsernameAndCategory(username, category);
         return ResultResponseDto.of(total, courses);
+    }
+
+    public ResultResponseDto getCultureCourses(String username, Domain domain) {
+        User user = userRepository.findByUserName(username)
+                .orElseThrow(() -> new CustomException(ErrorCode._UNAUTHORIZED));
+        List<Curriculum> curriculums = curriculumRepository.findAllByYearAndDomain(user.getYear(), domain);
+        List<Course> courses = courseRepository.findAllByUserUserNameAndSubDomainIsNotNull(username);
+        return ResultResponseDto.of(getSubDomains(curriculums), getSubDomains(courses));
+    }
+
+    private List<SubDomain> getSubDomains(List<? extends SubDomainHolder> holders) {
+        return holders.stream()
+                .map(SubDomainHolder::getSubDomain)
+                .toList();
     }
 }
