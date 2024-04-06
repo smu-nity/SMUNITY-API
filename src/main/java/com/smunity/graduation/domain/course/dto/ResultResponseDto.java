038621f4e6d38d3d999544d871ccccbd6ef466ef
hyunmin0317 <choihm9903@naver.com>
2024-04-06 17:56:06 +0900

    ✨ feat: ResultResponseDto of 메서드 작성 (#76)

--- src/main/java/com/smunity/graduation/domain/course/dto/ResultResponseDto.java
+++ src/main/java/com/smunity/graduation/domain/course/dto/ResultResponseDto.java
@@ -1,6 +1,7 @@
 package com.smunity.graduation.domain.course.dto;
 
 import com.smunity.graduation.domain.course.entity.Course;
+import com.smunity.graduation.global.common.enums.SubDomain;
 import lombok.Builder;
 
 import java.util.List;
@@ -16,14 +17,35 @@ public record ResultResponseDto(
     public static ResultResponseDto of(int total, List<Course> courses) {
         int completed = calculateCompleted(courses);
         return ResultResponseDto.builder()
-                .completed(total < completed)
+                .completed(total <= completed)
                 .status(StatusResponseDto.of(total, completed))
                 .count(courses.size())
                 .content(CourseResponseDto.from(courses))
                 .build();
     }
 
+    public static ResultResponseDto of(List<SubDomain> curriculums, List<SubDomain> courses) {
+        List<CultureResponseDto> cultures = CultureResponseDto.of(curriculums, courses);
+        int total = curriculums.size();
+        int completed = calculateCultureCompleted(cultures);
+        return ResultResponseDto.builder()
+                .completed(total <= completed)
+                .status(StatusResponseDto.of(total, completed))
+                .count(total)
+                .content(CultureResponseDto.of(curriculums, courses))
+                .build();
+    }
+
     private static int calculateCompleted(List<Course> courses) {
-        return courses.stream().mapToInt(Course::getCredit).sum();
+        return courses.stream()
+                .mapToInt(Course::getCredit)
+                .sum();
+    }
+
+    private static int calculateCultureCompleted(List<CultureResponseDto> cultures) {
+        return cultures.stream()
+                .filter(CultureResponseDto::completed)
+                .toList()
+                .size();
     }
 }
