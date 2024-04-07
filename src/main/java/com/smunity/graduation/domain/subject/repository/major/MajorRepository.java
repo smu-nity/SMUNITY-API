498c8454d2c6b095440d7f87b2f2a17f520e3f38
hyunmin0317 <choihm9903@naver.com>
2024-04-07 15:47:20 +0900

    ✨ feat: Major 리포지토리 작성 (#80)

---
+++ src/main/java/com/smunity/graduation/domain/subject/repository/major/MajorRepository.java
@@ -0,0 +1,8 @@
+package com.smunity.graduation.domain.subject.repository.major;
+
+import com.smunity.graduation.domain.subject.entity.Major;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MajorRepository extends JpaRepository<Major, Long> {
+    
+}
