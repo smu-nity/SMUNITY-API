0725b5d574591c514ef231bc94aead6f9aabc869
hyunmin0317 <choihm9903@naver.com>
2024-04-10 13:34:18 +0900

    ✨ feat: subject_culture 테이블 마이그레이션 sql (#85)

---
+++ src/main/resources/sql/subject_culture.sql
@@ -0,0 +1,186 @@
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_ENGINEER', 3, '창의적프로그래밍입문', 'HALF9319');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_ENGINEER', 3, '미래사회와디지털기술', 'HALF9329');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_ENGINEER', 3, '기초AI와콘텐츠', 'HALF9405');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_ENGINEER', 3, '플랫폼비즈니스와사회적가치', 'HALF9441');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BASIC_ACCIDENT', 3, '사고와표현', 'HALR1032');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_NATURAL', 3, '생명과학의이해', 'HALF0502');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_NATURAL', 3, '자연과학의세계', 'HALF0537');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_NATURAL', 3, '과학의철학적이해', 'HALF9041');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_NATURAL', 3, '대학수학', 'HALF9239');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_NATURAL', 3, '물리현상의이해', 'HALF9252');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_NATURAL', 3, '미래생활과화학', 'HALF9321');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_NATURAL', 3, '정량적사고', 'HALF9362');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_NATURAL', 3, '우주의역사', 'HALF9403');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('CORE_DIVERSITY', 2, '다른곳의세계와나', 'HALF9433');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('CORE_DIVERSITY', 3, '문화감수성의이해와실천', 'HALF9434');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('CORE_DIVERSITY', 3, '문화다양성과미디어', 'HALF9435');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('CORE_DIVERSITY', 2, '치유와공간', 'HALF9436');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BASIC_COMPUTER', 2, '컴퓨팅사고와데이터의이해', 'HALR1238');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BASIC_COMPUTER', 2, '알고리즘과게임콘텐츠', 'HALR1239');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BASIC_COMPUTER_2', 2, '알고리즘과게임콘텐츠', 'HALR1239');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_ART', 3, '미와예술의이해', 'HALF0601');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_ART', 3, '현대음악과문화', 'HALF0628');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_ART', 3, '오페라의이해와감상', 'HALF6071');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_ART', 3, '교향곡의이해', 'HALF6072');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_ART', 3, '현대생활과디자인', 'HALF7023');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_ART', 3, '현대미술의이해', 'HALF9061');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_ART', 3, '현대미술사와이론', 'HALF9356');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BASIC_ENG_MATH', 3, 'EnglishforAcademicPurposes', 'HALR1050');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BASIC_ENG_MATH', 3, '기초수학', 'HALR1231');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('CORE_CREATIVE', 2, '상상속의아이디어', 'HALF9427');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('CORE_CREATIVE', 2, '창의적문제해결', 'HALR1040');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('CORE_CREATIVE', 2, '컴퓨테이셔널씽킹', 'HALR1230');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('CORE_CREATIVE', 3, '창의적디자인상상', 'HALR1270');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_BRIDGE', 3, '철학으로읽는사회와문화', 'HALF9015');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_BRIDGE', 3, '현대사회와의사결정', 'HALF9407');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_BRIDGE', 3, '기초세계사', 'HALF9013');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_BRIDGE', 3, '인간과환경의이해', 'HALF6023');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_BRIDGE', 3, '인공지능의현재와미래', 'HALF9349');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_BRIDGE', 2, '기초과학과산업제품', 'HALF9327');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_BRIDGE', 3, '신소재와문명발달', 'HALF9378');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_BRIDGE', 3, '일본대중음악으로배우는일본사회일본어', 'HALF9444');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_BRIDGE', 3, '예술경영의이해', 'HALF9445');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_HUMANITIES', 3, '인간과언어', 'HALF0102');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_HUMANITIES', 3, '글로벌문화와문화콘텐츠', 'HALF0122');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_HUMANITIES', 3, '지구화시대의역사학과한국사의재조명', 'HALF0202');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_HUMANITIES', 3, '세계신화의이해', 'HALF0302');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_HUMANITIES', 3, '이야기속의논리와철학', 'HALF9014');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_HUMANITIES', 3, '명저읽기(일반)', 'HALF9302');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_HUMANITIES', 3, '명저읽기(과학,기술)', 'HALF9305');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_HUMANITIES', 3, '명저읽기(문학)', 'HALF9338');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_HUMANITIES', 3, '세계문화교류', 'HALF9340');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_HUMANITIES', 3, '휴먼커뮤니케이션', 'HALF9358');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_HUMANITIES', 3, '공간과문명,인간', 'HALF9374');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_HUMANITIES', 3, '사회문제해결을위한문화와콘텐츠', 'HALF9439');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_NATURAL_ENGINEER', 3, '생명과학의이해', 'HALF0502');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_NATURAL_ENGINEER', 3, '자연과학의세계', 'HALF0537');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_NATURAL_ENGINEER', 3, '과학의철학적이해', 'HALF9041');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_NATURAL_ENGINEER', 3, '대학수학', 'HALF9239');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_NATURAL_ENGINEER', 3, '물리현상의이해', 'HALF9252');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_NATURAL_ENGINEER', 3, '미래생활과화학', 'HALF9321');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_NATURAL_ENGINEER', 3, '정량적사고', 'HALF9362');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_NATURAL_ENGINEER', 3, '우주의역사', 'HALF9403');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_NATURAL_ENGINEER', 3, '창의적프로그래밍입문', 'HALF9319');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_NATURAL_ENGINEER', 3, '미래사회와디지털기술', 'HALF9329');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_NATURAL_ENGINEER', 3, '기초AI와콘텐츠', 'HALF9405');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_NATURAL_ENGINEER', 3, '플랫폼비즈니스와사회적가치', 'HALF9441');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BASIC_COMPUTER_1', 2, '컴퓨팅사고와데이터의이해', 'HALR1238');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('CORE_ETHICAL', 2, '호모엠파티쿠스(고통받는인간)', 'HALF9404');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('CORE_ETHICAL', 3, '과학기술자의직업윤리', 'HALF9437');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('CORE_ETHICAL', 2, '인간과시간', 'HALF9438');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('CORE_ETHICAL', 2, '상명정신과윤리적삶', 'HALR1038');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('CORE_PROFESSIONAL', 3, '이미지시대의테크놀로지와예술', 'HALF9398');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('CORE_PROFESSIONAL', 3, '빅히스토리와인공지능', 'HALF9408');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('CORE_PROFESSIONAL', 3, '과학적사고와탐구', 'HALF9425');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('CORE_PROFESSIONAL', 3, '예술,비즈니스,문화와함께하는지식재산권법입문', 'HALR1269');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_SOCIAL', 3, '인구와사회', 'HALF0447');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_SOCIAL', 3, '경제학의이해', 'HALF4033');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_SOCIAL', 3, '글로벌경제의이해', 'HALF5013');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_SOCIAL', 3, '법과민주주의', 'HALF9030');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_SOCIAL', 3, '현대사회와심리학', 'HALF9031');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_SOCIAL', 3, '현대사회와인간', 'HALF9245');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_SOCIAL', 3, '현대정치의이해', 'HALF9266');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_SOCIAL', 3, '의식주와소비문화', 'HALF9280');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_SOCIAL', 2, '리터러시탐구와응용', 'HALF9320');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_SOCIAL', 3, '창의적사고의프레임워크', 'HALF9326');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_SOCIAL', 3, '세계종교와문화', 'HALF9343');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_SOCIAL', 3, '인터넷윤리', 'HALF9379');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_SOCIAL', 2, '현대사회와윤리', 'HALF9440');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('BALANCE_SOCIAL', 2, '문화다양성과글로벌시민', 'HALR1041');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('CORE_CONVERGENCE', 3, 'K-Culture를통해보는예술의산업가치', 'HALF9429');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('CORE_CONVERGENCE', 3, '영화속의건축여행', 'HALF9431');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('CORE_CONVERGENCE', 2, '융합창의수학', 'HALF9432');
+INSERT INTO subject_culture (sub_domain, credit, name, number)
+VALUES ('CORE_CONVERGENCE', 3, '색채심리학', 'HALF1271');
