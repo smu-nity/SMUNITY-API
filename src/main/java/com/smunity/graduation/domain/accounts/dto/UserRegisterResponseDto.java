d9cd30750d14ff7499d96f9abce29570040ea278
hyunmin0317 <choihm9903@naver.com>
2024-04-18 21:03:13 +0900

    🐛 fix: 회원가입 API Response id 값 null 오류 (#88)

--- src/main/java/com/smunity/graduation/domain/accounts/dto/UserRegisterResponseDto.java
+++ src/main/java/com/smunity/graduation/domain/accounts/dto/UserRegisterResponseDto.java
@@ -1,31 +1,31 @@
 package com.smunity.graduation.domain.accounts.dto;
 
 import com.smunity.graduation.domain.accounts.entity.User;
-
 import lombok.Builder;
 
 @Builder
 public record UserRegisterResponseDto(
-	Long id,
-	String email,
-	String userName,
-	String name,
-	String year,
-	String department,
-	int currentYear,
-	int completedSemesters
+        Long id,
+        String email,
+        String userName,
+        String name,
+        String year,
+        String department,
+        int currentYear,
+        int completedSemesters
 
 ) {
 
-	public static UserRegisterResponseDto from(User user) {
-		return UserRegisterResponseDto.builder()
-			.email(user.getEmail())
-			.userName(user.getUserName())
-			.name(user.getName())
-			.year(user.getYear().getName())
-			.department(user.getDepartment().getName())
-			.currentYear(user.getCurrentYear())
-			.completedSemesters(user.getCompletedSemesters())
-			.build();
-	}
+    public static UserRegisterResponseDto from(User user) {
+        return UserRegisterResponseDto.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .userName(user.getUserName())
+                .name(user.getName())
+                .year(user.getYear().getName())
+                .department(user.getDepartment().getName())
+                .currentYear(user.getCurrentYear())
+                .completedSemesters(user.getCompletedSemesters())
+                .build();
+    }
 }
