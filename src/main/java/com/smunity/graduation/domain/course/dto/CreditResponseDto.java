761f9f59b89fda442076165d551ab7ff175d2bac
hyunmin0317 <choihm9903@naver.com>
2024-04-20 17:34:26 +0900

    ✨ feat: CreditResponse Dto 필드 추가 (#91)

--- src/main/java/com/smunity/graduation/domain/course/dto/CreditResponseDto.java
+++ src/main/java/com/smunity/graduation/domain/course/dto/CreditResponseDto.java
@@ -1,12 +1,16 @@
 package com.smunity.graduation.domain.course.dto;
 
+import com.smunity.graduation.domain.accounts.entity.User;
 import lombok.Builder;
 
 import static com.smunity.graduation.domain.course.dto.StatusResponseDto.calculateCompletion;
 import static com.smunity.graduation.domain.course.dto.StatusResponseDto.calculateRequired;
+import static com.smunity.graduation.global.common.type.Category.*;
 
 @Builder
 public record CreditResponseDto(
+        String userName,
+        String name,
         int total,
         int completed,
         int major,
@@ -16,8 +20,14 @@ public record CreditResponseDto(
         int completion
 ) {
 
-    public static CreditResponseDto of(int total, int completed, int major, int culture) {
+    public static CreditResponseDto from(User user) {
+        int total = user.getYear().getTotal();
+        int completed = user.getCompletedCredits();
+        int major = user.getCompletedCredits(MAJOR_ADVANCED) + user.getCompletedCredits(MAJOR_OPTIONAL);
+        int culture = user.getCompletedCredits(CULTURE);
         return CreditResponseDto.builder()
+                .userName(user.getUserName())
+                .name(user.getName())
                 .total(total)
                 .completed(completed)
                 .major(major)
