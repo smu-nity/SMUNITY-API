3c2096d4fe6fe59cb8a241f64109b604eecb8f41
hyunmin0317 <choihm9903@naver.com>
2024-04-29 21:12:10 +0900

    ✨ feat: 답변 관련 API 응답 스펙 변경 (#95)

--- src/main/java/com/smunity/graduation/domain/qna/service/AnswerQueryService.java
+++ src/main/java/com/smunity/graduation/domain/qna/service/AnswerQueryService.java
@@ -1,26 +1,24 @@
 package com.smunity.graduation.domain.qna.service;
 
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.smunity.graduation.domain.accounts.exception.AccountsExceptionHandler;
 import com.smunity.graduation.domain.qna.dto.AnswerResponseDto;
 import com.smunity.graduation.domain.qna.entity.Answer;
 import com.smunity.graduation.domain.qna.repository.AnswerJpaRepository;
 import com.smunity.graduation.global.common.ErrorCode;
-
+import com.smunity.graduation.global.common.exception.CustomException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 @Service
 public class AnswerQueryService {
 
-	private final AnswerJpaRepository answerJpaRepository;
+    private final AnswerJpaRepository answerJpaRepository;
 
-	public AnswerResponseDto getAnswer(Long questionId) {
-		Answer answer = answerJpaRepository.findByQuestionId(questionId)
-			.orElseThrow(() -> new AccountsExceptionHandler(ErrorCode.QUESTION_NOT_FOUND));
-		return AnswerResponseDto.from(answer);
-	}
+    public AnswerResponseDto getAnswer(Long questionId) {
+        Answer answer = answerJpaRepository.findByQuestionId(questionId)
+                .orElseThrow(() -> new CustomException(ErrorCode.ANSWER_NOT_FOUND));
+        return AnswerResponseDto.from(answer);
+    }
 }
